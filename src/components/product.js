e27ccbc0d9f1618f876d09d3e8ecdb52767f09ad
andriiko <andriiko@wix.com>
2020-10-06 21:30:14 +0300

    add custom hook

--- src/components/product.js
+++ src/components/product.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import useAmount from '../hooks/use-amount';
 
 export default function Product(props) {
+  const { count, increment, decrement } = useAmount(2);
+
   return (
     <div>
       <p>{props.product.name}</p>
       <p>{props.product.price} $</p>
+      <button onClick={decrement}>-</button>
+      {count}
+      <button onClick={increment}>+</button>
     </div>
   );
 }
