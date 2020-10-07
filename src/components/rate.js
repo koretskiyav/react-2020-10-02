85bfcfddcd98f4262b4b2e0dea5e92fbfdc3b8e0
Oleg Popov <olegtwice@gmail.com>
2020-10-07 19:09:45 +0300

    Revert "Add Restaurant, Rate, Review components"
    This reverts commit 69f12f4426818dcaba5a247dc285e6d85dda6032.

--- src/components/rate.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Rate({ rating }) {
-  return (
-    <span>
-      <strong>{rating ? rating : 'No votes'}</strong>
-    </span>
-  );
-}
--- src/components/restaurants.js
+++ src/components/restaurants.js
@@ -1,22 +1,22 @@
 import React, { useState, useMemo } from 'react';
+import Menu from './menu';
 import Navigation from './navigation';
-import Restaurant from './restaurant';
 
-export default function Restaurants({ restaurants }) {
-  const [activeId, setActiveId] = useState(restaurants[0].id);
+export default function Restaurants(props) {
+  const [activeId, setActiveId] = useState(props.restaurants[0].id);
 
   const activeRestaurant = useMemo(
-    () => restaurants.find((restaurant) => restaurant.id === activeId),
-    [restaurants, activeId]
+    () => props.restaurants.find((restaurant) => restaurant.id === activeId),
+    [props.restaurants, activeId]
   );
 
   return (
     <div>
-      <Navigation restaurants={restaurants} onRestaurantClick={setActiveId} />
-      <Restaurant
-        reviews={activeRestaurant.reviews}
-        menu={activeRestaurant.menu}
+      <Navigation
+        restaurants={props.restaurants}
+        onRestaurantClick={setActiveId}
       />
+      <Menu menu={activeRestaurant.menu} />
     </div>
   );
 }
