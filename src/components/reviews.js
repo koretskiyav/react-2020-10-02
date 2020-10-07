16bb9eefa4cf054a511dfbe732eb929ca7166a8d
Vitaly Bibik <wital2010@gmail.com>
2020-10-07 16:59:49 +0300

    Repair Rate

--- src/components/reviews.js
+++ src/components/reviews.js
@@ -2,8 +2,16 @@ import React from 'react';
 import Rate from './rate';
 
 function Reviews(props) {
-  console.log('reviews', props);
-  return <Rate rate={props.reviews} />;
+  const ratingLength = props.reviews.length;
+  let sumRating = 0;
+  props.reviews.map((el) => {
+    return (sumRating += el.rating);
+  });
+  const finalRate = Math.floor(sumRating / ratingLength);
+
+  console.log(finalRate);
+
+  return <Rate rate={finalRate} />;
 }
 
 export default Reviews;
