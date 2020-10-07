f74791fdb60ed131bb155a1a19550f08bd6cff8e
Vitalik <wital2010@gmail.com>
2020-10-07 21:10:03 +0300

    Redone task 1

--- src/components/reviews.js
+++ src/components/reviews.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Rate from './rate';
 
 function Reviews(props) {
-  const ratingLength = props.reviews.length;
-  let sumRating = 0;
-  props.reviews.map((el) => {
-    return (sumRating += el.rating);
-  });
-  const finalRate = Math.floor(sumRating / ratingLength);
+  // const ratingLength = props.reviews.length;
+  // let sumRating = 0;
+  //
+  // const activeSum = useMemo(
+  //     () => props.reviews.map((el) => {
+  //       return (sumRating += el.rating);
+  //     }), [props.reviews, sumRating]
+  // );
+  // const finalRate = Math.floor(sumRating / ratingLength);
+  //
+  // console.log(finalRate);
 
-  console.log(finalRate);
+  const finalRate = useMemo(
+    () =>
+      Math.floor(
+        props.reviews.reduce((acc, item) => acc + item.rating, 0) /
+          props.reviews.length
+      ),
+    [props.reviews]
+  );
 
   return <Rate rate={finalRate} />;
 }
