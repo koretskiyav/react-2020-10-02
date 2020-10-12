8f6c49dd6a62a6e44f4f8f7b44cab9af6bd81d13
Anton Fedorov <anton.pzkt@gmail.com>
2020-10-12 20:34:27 +0300

    added to useAmount() props.amount

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -39,12 +39,12 @@ class Menu extends React.Component {
   }
 }
 
-Menu.propTypes = {
+/*Menu.propTypes = {
   menu: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
     }).isRequired
   ).isRequired,
-};
+};*/
 
 export default Menu;
