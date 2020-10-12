f9a9b67c7421e409db21d9e07705d872b308e8ea
Kostiantyn Loboda <kloboda@gmail.com>
2020-10-12 17:11:04 +0300

    HT2_PropTypes

--- src/components/restaurant/restaurant.js
+++ src/components/restaurant/restaurant.js
@@ -1,4 +1,5 @@
 import React, { useMemo } from 'react';
+import PropTypes, {arrayOf} from 'prop-types';
 import Menu from '../menu';
 import Reviews from '../reviews';
 import Banner from '../banner';
@@ -25,5 +26,15 @@ const Restaurant = ({ restaurant }) => {
     </div>
   );
 };
+
+Restaurant.propTypes = {
+    restaurant: PropTypes.shape({
+        name: PropTypes.string,
+        menu: arrayOf(PropTypes.object),
+        reviews: arrayOf(PropTypes.shape({
+            rating: PropTypes.number,
+        }).isRequired).isRequired,
+    }).isRequired
+}
 
 export default Restaurant;
