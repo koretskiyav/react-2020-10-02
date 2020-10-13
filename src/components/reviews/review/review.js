6e442a074ae1c75777e3ab52be83f99396561d95
AlexDayanov <ddhgate@gmail.com>
2020-10-13 11:57:14 +0500

    Tests for Review component added

--- src/components/reviews/review/review.js
+++ src/components/reviews/review/review.js
@@ -7,11 +7,15 @@ const Review = ({ user, text, rating }) => (
   <div className={styles.review}>
     <div className={styles.content}>
       <div>
-        <h4 className={styles.name}>{user}</h4>
-        <p className={styles.comment}>{text}</p>
+        <h4 className={styles.name} data-id="review-user-name">
+          {user}
+        </h4>
+        <p className={styles.comment} data-id="review-text">
+          {text}
+        </p>
       </div>
       <div className={styles.rate}>
-        <Rate value={rating} />
+        <Rate value={rating} data-id="review-rating" />
       </div>
     </div>
   </div>
--- src/components/reviews/reviews.test.js
+++ src/components/reviews/reviews.test.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Reviews from './reviews';
+import Review from './review';
 
 import { restaurants } from '../../fixtures';
 
 const reviews = restaurants[0].reviews;
 const review = reviews[1];
+const { user, text, rating } = review;
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -16,15 +18,31 @@ describe('Reviews', () => {
     expect(wrapper.find('[data-id="review"]').length).toBe(2);
   });
 
-  it('should render 5 stars', () => {
+  it('should render all stars', () => {
     const wrapper = mount(<Reviews reviews={[review]} />);
     expect(wrapper.find('[data-id="rate-star"]').length).toBe(5);
   });
-  it('sould render 3 stars checked', () => {
+  it('should render stars checked', () => {
     const wrapper = mount(<Reviews reviews={[review]} />);
     const checked = wrapper
       .find('[data-id="rate-star"]')
       .filterWhere((n) => n.props().checked);
-    expect(checked.length).toBe(3);
+    expect(checked.length).toBe(rating);
+  });
+  it('should render correct user-text', () => {
+    const wrapper = mount(<Reviews reviews={[review]} />);
+    expect(wrapper.find('[data-id="review-text"]').text()).toBe(text);
+  });
+  it('should render correct user-name', () => {
+    const wrapper = mount(<Reviews reviews={[review]} />);
+    expect(wrapper.find('[data-id="review-user-name"]').text()).toBe(user);
+  });
+  it('should render zero rating by default', () => {
+    const wrapper = mount(<Review user={user} text={text} />);
+    expect(
+      wrapper
+        .find('[data-id="review-rating"]')
+        .filterWhere((n) => n.props().checked).length
+    ).toBe(0);
   });
 });
