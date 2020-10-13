4f0f822fc67ec46951af759935d0361a9bdb2e5c
andriiko <andriiko@wix.com>
2020-10-13 21:25:21 +0300

    add simple redux flow

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import styles from './product.module.css';
 import MinusIcon from './icons/minus.svg';
 import PlusIcon from './icons/plus.svg';
 
-import counter from '../../hocs/counter';
+import { increment, decrement } from '../../redux/actions';
 
 const Product = ({ product, amount, increment, decrement, fetchData }) => {
   useEffect(() => {
@@ -60,4 +61,18 @@ Product.propTypes = {
   increment: PropTypes.func,
 };
 
-export default counter(Product);
+const mapStateToProps = (state) => ({
+  amount: state.order,
+});
+
+const mapDispatchToProps = {
+  decrement,
+  increment,
+};
+
+// const mapDispatchToProps = (dispatch) => ({
+//   decrement: () => dispatch(decrement()),
+//   increment: () => dispatch(increment()),
+// });
+
+export default connect(mapStateToProps, mapDispatchToProps)(Product);
