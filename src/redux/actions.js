ffcb10118ed63020e8e2c727656ad356b503ca7a
Anton Fedorov <anton.pzkt@gmail.com>
2020-10-16 01:28:35 +0300

    added new actions and constants: removing - remove product from basket and clear - clear basket

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,6 @@
-import { INCREMENT, DECREMENT } from './constants';
+import { INCREMENT, DECREMENT, REMOVING, CLEAR } from './constants';
 
 export const increment = (id) => ({ type: INCREMENT, payload: { id } });
 export const decrement = (id) => ({ type: DECREMENT, payload: { id } });
+export const removing = (id) => ({ type: REMOVING, payload: { id } });
+export const clear = () => ({ type: CLEAR, payload: {} });
--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,2 +1,4 @@
 export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
+export const REMOVING = 'REMOVING';
+export const CLEAR = 'CLEAR';
