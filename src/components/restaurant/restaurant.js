7d5b777b200788cd5cf8056343273c0fddb43c7b
Vitalik <wital2010@gmail.com>
2020-10-19 21:15:40 +0300

    HomeTask is Done, not to work Anonymous on form

--- src/components/restaurant/restaurant.js
+++ src/components/restaurant/restaurant.js
@@ -1,22 +1,25 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Menu from '../menu';
 import Reviews from '../reviews';
 import Banner from '../banner';
 import Rate from '../rate';
 import Tabs from '../tabs';
+import { connect } from 'react-redux';
+import {
+  restaurantAverageRatingSelector,
+  restaurantReviewsSelector,
+} from '../../redux/selectors';
 
-const Restaurant = ({ restaurant }) => {
-  const { name, menu, reviews } = restaurant;
-
-  const averageRating = useMemo(() => {
-    const total = reviews.reduce((acc, { rating }) => acc + rating, 0);
-    return Math.round(total / reviews.length);
-  }, [reviews]);
+const Restaurant = ({ restaurant, averageRating }) => {
+  const { name, menu, reviews, id } = restaurant;
 
   const tabs = [
     { title: 'Menu', content: <Menu menu={menu} /> },
-    { title: 'Reviews', content: <Reviews reviews={reviews} /> },
+    {
+      title: 'Reviews',
+      content: <Reviews reviewIds={reviews} restaurantId={id} />,
+    },
   ];
 
   return (
@@ -33,12 +36,13 @@ Restaurant.propTypes = {
   restaurant: PropTypes.shape({
     name: PropTypes.string,
     menu: PropTypes.array,
-    reviews: PropTypes.arrayOf(
-      PropTypes.shape({
-        rating: PropTypes.number.isRequired,
-      }).isRequired
-    ).isRequired,
+    reviews: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
   }).isRequired,
 };
 
-export default Restaurant;
+const mapStateToProps = (state, ownProps) => ({
+  reviews: restaurantReviewsSelector(state, ownProps),
+  averageRating: restaurantAverageRatingSelector(state, ownProps),
+});
+
+export default connect(mapStateToProps)(Restaurant);
--- src/components/reviews/review/review.js
+++ src/components/reviews/review/review.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import Rate from '../../rate';
 import styles from './review.module.css';
+import { connect } from 'react-redux';
+import { reviewSelector, userSelector } from '../../../redux/selectors';
 
 const Review = ({ user, text, rating }) => (
   <div className={styles.review} data-id="review">
@@ -32,4 +33,15 @@ Review.defaultProps = {
   user: 'Anonymous',
 };
 
-export default Review;
+const mapStateToProps = (state, ownProps) => {
+  const { rating, text, userId } = reviewSelector(state, ownProps.id);
+  const user = userSelector(state, userId);
+
+  return {
+    rating,
+    text,
+    user: user.name,
+  };
+};
+
+export default connect(mapStateToProps)(Review);
