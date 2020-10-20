919876f015d874e386ec1aa7945a49e0d5d90ac0
andriiko <andriiko@wix.com>
2020-10-20 13:24:00 +0300

    add HW4

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 import styles from './product.module.css';
 
 import { increment, decrement } from '../../redux/actions';
 
 import Button from '../button';
+import { productAmountSelector, productSelector } from '../../redux/selectors';
 
 const Product = ({ product, amount, increment, decrement, fetchData }) => {
   useEffect(() => {
@@ -57,19 +59,19 @@ Product.propTypes = {
   increment: PropTypes.func,
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  amount: state.order[ownProps.id] || 0,
-  product: state.products[ownProps.id],
+const mapStateToProps = createStructuredSelector({
+  amount: productAmountSelector,
+  product: productSelector,
 });
+
+// const mapStateToProps = (state, props) => ({
+//   amount: productAmountSelector(state, props),
+//   product: productSelector(state, props),
+// });
 
 const mapDispatchToProps = {
   decrement,
   increment,
 };
-
-// const mapDispatchToProps = (dispatch) => ({
-//   decrement: () => dispatch(decrement()),
-//   increment: () => dispatch(increment()),
-// });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Product);
