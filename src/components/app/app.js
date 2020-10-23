12d72b2716465086cb271ef7d76d926cda0061f6
andriiko <andriiko@wix.com>
2020-10-23 21:27:36 +0300

    add restaurants page

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,8 +1,8 @@
 import React, { PureComponent } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Restaurants from '../restaurants';
 import Header from '../header';
 import Basket from '../basket';
+import RestaurantsPage from '../../pages/restaurants-page';
 
 export default class App extends PureComponent {
   render() {
@@ -12,7 +12,7 @@ export default class App extends PureComponent {
         <Switch>
           <Route path="/" exact component={() => 'main page'} />
           <Route path="/checkout" component={Basket} />
-          <Route path="/restaurants/:restId" component={Restaurants} />
+          <Route path="/restaurants" component={RestaurantsPage} />
           <Route path="/" component={() => '404 - not found'} />
         </Switch>
       </div>
