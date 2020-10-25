d3f4353c9f539a9e53deedf2a1c9117d43191edd
Timur Guslov <timur.guslov@gmail.com>
2020-10-25 12:01:14 +0500

    basket product made link to restaurant

--- src/components/basket/basket-item/basket-item.js
+++ src/components/basket/basket-item/basket-item.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import cn from 'classnames';
 import { increment, decrement, remove } from '../../../redux/actions';
@@ -11,12 +12,13 @@ function BasketItem({
   subtotal,
   increment,
   decrement,
+  restaurantId,
   remove,
 }) {
   return (
     <div className={styles.basketItem}>
       <div className={styles.name}>
-        <span>{product.name}</span>
+        <Link to={`/restaurants/${restaurantId}`}>{product.name}</Link>
       </div>
       <div className={styles.info}>
         <div className={styles.counter}>
