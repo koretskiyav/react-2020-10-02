03443a9d4dfd3be92a5f8935d2f8f3ed34ad121c
Yury Vinogradov <yuvinogradov@gmail.com>
2020-10-26 20:59:00 +0200

    add paths to menu and review tabs for restaurants

--- src/components/tabs/tabs.js
+++ src/components/tabs/tabs.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
+import { NavLink } from 'react-router-dom';
 
 import styles from './tabs.module.css';
 
@@ -8,18 +9,20 @@ const Tabs = ({ tabs }) => {
   const [activeTab, setActiveTab] = useState(0);
 
   const { content } = tabs[activeTab];
+  const { restaurantId } = tabs[activeTab].content.props;
 
   return (
     <>
       <div className={styles.tabs}>
         {tabs.map(({ title }, index) => (
-          <span
+          <NavLink
             key={title}
+            to={`/restaurants/${restaurantId}/${title}`}
             className={cn(styles.tab, { [styles.active]: index === activeTab })}
             onClick={() => setActiveTab(index)}
           >
             {title}
-          </span>
+          </NavLink>
         ))}
       </div>
       {content}
